3e576d130390be9ef5cba85ab55e04405c83d47c
rizalkalam <rizalkalam78@gmail.com>
2025-05-15 22:52:04 +0700

    Test

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,6 @@
 // lib/main.dart
 import 'package:flutter/material.dart';
+import 'package:frontend/ui/pages/dashboard/profile/profile_page.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frontend/blocs/user_bloc.dart';
@@ -96,7 +97,7 @@ class MyApp extends StatelessWidget {
               ),
             ),
           ),
-          home: const SplashPage(),
+          home: const ProfilePage(),
         ),
       ),
     );
